0dfe66a988deb77904ab7b22bb532fc9d484af5c
RafaelLopez <rafa.lophdz7@gmail.com>
2020-10-21 19:01:04 -0500

    filtro verde hecho, falta anexar al menu y llamar solo la funcion

--- filtroVerdes.js
+++ filtroVerdes.js
@@ -1,28 +1,27 @@
 (function(){
-<canvas id="img" width="225" height="225"></canvas>
+	<canvas id="img" width="225" height="225"></canvas>
+		let img=document.getElementById("img");
+		let contexto= img.getContext("2d");
 
-    var img=document.getElementById("img");
-    var contexto= img.getContext("2d");
+		 let foto= new Image();
+		 foto.onload=function(){
+		 	contexto.drawImage(foto, 0 ,0);
+		 }
+		 foto.src = "i.jpeg";
+		 function verde(){
+		 	let numPixel=contexto.getImageData(0,0,225,225);
+		 	let contPixel= numPixel.width * numPixel.height;
+              for ( let i = 0; i < contPixel; i++ ) {
+              			let v = numPixel.data[ i*4+1];
 
-     var foto= new Image();
-     foto.onload=function(){
-         contexto.drawImage(foto, 0 ,0);
-     }
-     foto.src = "i.jpeg";
-     function verde(){
-         var numPixel=contexto.getImageData(0,0,225,225);
-         var contPixel= numPixel.width * numPixel.height;
-          for ( var i = 0; i < contPixel; i++ ) {
-                      var v = numPixel.data[ i*4+1];
+              			numPixel.data[i*4]=0;
+              			numPixel.data[i*4+1]=v;
+               			numPixel.data[i*4+2]=0;
+		 }
+		 contexto.putImageData( numPixel, 0, 0 );
+		};
 
-                      numPixel.data[i*4]=0;
-                      numPixel.data[i*4+1]=v;
-                       numPixel.data[i*4+2]=0;
-     }
-     contexto.putImageData( numPixel, 0, 0 );
-    };
-    
-<div>
-    <input type="button" onclick="verde()" value="VERDE" name="">
-</div>
-});
+	//<div>
+        <input type="button" onclick="verde()" value="VERDE" name=""></input>
+        //</div>
+    })
