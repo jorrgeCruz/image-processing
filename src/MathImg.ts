a64136ac86889e877af8a7a66c88df9fba013ece
Jorge Cruz <jorrge.cruz@gmail.com>
2020-10-27 11:38:31 -0600

    Cambios parciales de nueva operacion

--- src/MathImg.ts
+++ src/MathImg.ts
@@ -69,10 +69,20 @@ export class MathImg{
     return arrImage;
   }
 
+  public static toUmbral(img: ImageType, umbral: number): number[][][] {
+    //variable que guarda el arreglo 3d de la imagen de color
+    var arrImage: number[][][] = img.getArrayImg();
+    //variable donde guardamos la salida
+    var sal: number[][][] = this.initArray(img.getWidth(), img.getHeight());
+    for (let i = 0; i < img.getHeight(); i++){
+      for (let j = 0; j < img.getWidth(); j++) { 
+        sal[0][i][j] = arrImage[0][i][j] > umbral ? 255 : 0;
+        sal[1][i][j] = arrImage[1][i][j] > umbral ? 255 : 0;
+        sal[2][i][j] = arrImage[2][i][j] > umbral ? 255 : 0;
+        
+      }
+    } 
+    return sal;
+  }
+
 }
-
-
-/**prom = (arrImage[0][i][j] + arrImage[1][i][j] + arrImage[2][i][j]) /3;
-        sal[0][i][j] = prom;
-        sal[1][i][j] = prom;
-        sal[2][i][j] = prom; */
