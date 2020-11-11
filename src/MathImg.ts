e287241597ab6ee739774fdba0e2940f1c7d30a1
UrielMorales <urielmorales@gmail.com>
2020-11-11 13:41:20 -0600

    se agrego el Contraste

--- src/MathImg.ts
+++ src/MathImg.ts
@@ -211,4 +211,31 @@ export class MathImg{
     } 
     return sal;
   }
+  public static changeContraste(img: ImageType, valor: number): number[][][] {
+    //variable que guarda el arreglo 3d de la imagen de color
+    var arrImage: number[][][] = img.getArrayImg();
+    //variable donde guardamos la salida
+    var sal: number[][][] = this.initArray(img.getWidth(), img.getHeight());
+    //
+    var cR, cG, cB: number;
+    var contraste: number;
+    contraste = (valor + 100) / 100;
+    for (let i = 0; i < img.getHeight(); i++){
+      for (let j = 0; j < img.getWidth(); j++) { 
+        cR = ((((arrImage[0][i][j]/255.0) - 0.5) * contraste) + 0.5) * 255.0;
+        if (cR > 255) cR = 255;
+        if (cR < 0) cR = 0;
+        sal[0][i][j] = cR;
+        cG = ((((arrImage[1][i][j]/255.0) - 0.5) * contraste) + 0.5) * 255.0;
+        if (cG > 255) cG = 255;
+        if (cG < 0) cG = 0;
+        sal[1][i][j] = cG;
+        cB = ((((arrImage[2][i][j]/255.0) - 0.5) * contraste) + 0.5) * 255.0;
+        if (cB > 255) cB = 255;
+        if (cB < 0) cB = 0;
+        sal[2][i][j] = cB;
+      }
+    } 
+    return sal; 
+  }
 }
