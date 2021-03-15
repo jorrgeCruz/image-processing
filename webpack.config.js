9d5357577a5a463e4bbeb37bdeefb2fad8c31132
Jorge Cruz <jorrge.cruz@gmail.com>
2021-03-15 12:59:06 -0600

    Actualizacion del html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./dist/src/index.js",
     output: {
-        path: path.resolve(__dirname, "src"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".js"]
     }
 }
