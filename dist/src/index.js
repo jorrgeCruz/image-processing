850b79f8af8f0493ebfc866f183599038ee71c91
Dominguez <pdominguezsantiago@gmail.com>
2021-04-27 20:23:19 -0500

    se agregaron las funciones del index

--- dist/src/index.js
+++ dist/src/index.js
@@ -207,6 +207,21 @@ function rain2(evt) {
     init();
     animate2();
 }
+function Angular() {
+    ctx.drawImage(imgLocal.getImage(), 0, 0, w, h);
+    ctx.globalAlpha = 0.25;
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0, 0, w, h);
+    for (var i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].updateAngular();
+        particlesArray[i].draw();
+    }
+    requestAnimationFrame(Angular);
+}
+function rainAngular(evt) {
+    init();
+    Angular();
+}
 lienzo1.addEventListener("mousemove", imgLocal.drawSmallImg);
 document.getElementById('files').addEventListener('change', imgLocal.handleFileSelect, false);
 document.getElementById('files2').addEventListener('change', imgLocal4.handleFileSelect, false);
@@ -243,3 +258,4 @@ document.getElementById("op-addimg").addEventListener('click', sumaImg, false);
 //op con efectos
 document.getElementById("op-rain").addEventListener('click', rain, false);
 document.getElementById("op-rain2").addEventListener('click', rain2, false);
+document.getElementById("op-rainAngular").addEventListener('click', rainAngular, false);
