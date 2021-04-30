899b6bb9c7e2858ea30149c1a05b00678d33e45d
Gabriel <gs298300@gmail.com>
2021-04-30 15:38:00 -0500

    se agrego horizontalRL

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-
 import { ImageLocal } from "./ImageLocal.js";
 import { ImageType } from "./ImageType.js";
 import { MathImg } from "./MathImg.js";
 import { Particle } from "./particle.js";
-import { ParticleText } from "./particle.js";
 
 let lienzo1: HTMLCanvasElement;
 let lienzo2: HTMLCanvasElement;
@@ -176,7 +174,6 @@ let h:number;
 const numberOfParticles = 5000;
 let particlesArray: Particle[];
 particlesArray = new Array(0);
-var imagenSal: ImageType;
 
 function init() {
   //init
@@ -223,56 +220,21 @@ function rain2(evt: any): void {
   animate2();
 }
 
-//codigo para efecto de particulas
-let particleArray: ParticleText[];
-let mouse:any = {
-  x: null,
-  y: null,
-  radius: 150
-};
-
-function handleMouse(e: any) {
-  mouse.x = e.x;// - canvasPosition.left;
-  mouse.y = e.y;// - canvasPosition.top;
-  //console.log(mouse.x, mouse.y)
-}
-
-function textEfects(evt: any): void{
-  var args = prompt("Ingresa texto, tamaño de texto y coord x y y, separados por coma:");
-  
-  var factores = args.split(',');//.map(elem => parseInt(elem));
-  pantalla1.font = 'bold  ' + factores[1] + 'px Verdana';
-  //let cadena = 
-  
-  pantalla1.fillText(factores[0], parseInt(factores[2]), parseInt(factores[3]));
-  imagenSal = new ImageType(pantalla1, null, 300, 300, true);
-  initParticles();
-  animateParticles();
-}
-
-function initParticles() {
-  particleArray = [];
-  let arrImage = imagenSal.getArrayImg();
-  for (let i = 0; i < 300; i++){
-    for (let j = 0; j < 300; j++) { 
-      if (arrImage[0][i][j] > 128) {
-        particleArray.push(new ParticleText(j, i, pantalla1));
-      }
-    }
-  } 
-}
-
-function animateParticles(){
-  pantalla1.clearRect(0,0,300,300);
-  for (let i = 0; i < particleArray.length; i++){
-      particleArray[i].update(mouse);
-      particleArray[i].draw();
+function HorizontalRL() {
+  ctx.drawImage(imgLocal.getImage(), 0, 0, w, h);
+  ctx.globalAlpha = 0.25;
+  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillRect(0, 0, w, h);
+  for (let i = 0; i < particlesArray.length; i++){
+    particlesArray[i].updaterainHorizontal();
+    particlesArray[i].draw();
   }
-  requestAnimationFrame(animateParticles);
+  requestAnimationFrame(HorizontalRL);
 }
-
-
-lienzo1.addEventListener('mousemove', handleMouse);
+function rainHorizontal(evt: any): void { 
+  init();
+  HorizontalRL();
+}
 
 lienzo1.addEventListener("mousemove", imgLocal.drawSmallImg);
 document.getElementById('files').addEventListener('change', imgLocal.handleFileSelect, false);
@@ -315,7 +277,4 @@ document.getElementById("op-addimg").addEventListener('click', sumaImg, false);
 //op con efectos
 document.getElementById("op-rain").addEventListener('click', rain, false);
 document.getElementById("op-rain2").addEventListener('click', rain2, false);
-
-
-//op con texto.
-document.getElementById("op-text").addEventListener('click', textEfects, false);
+document.getElementById("op-HorizontalRL").addEventListener('click', rainHorizontal, false);
