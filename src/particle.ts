13256058ecc7399ce4fe33752715b3c294db6706
dali <dali@gmail.com>
2021-05-05 15:09:44 -0500

    S e Agrego el efecto lluvia horizontal de izquierda a derecha

--- src/particle.ts
+++ src/particle.ts
@@ -46,6 +46,22 @@ export class Particle {
     }
   }
 
+  
+  public HorizontalLR() {
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
+      let movement = 0;
+      if (this.x < this.height) {
+        this.speed = this.mappedImage[0][this.position1][this.position2];
+        movement = (2.5 - this.speed) + this.velocity;
+      }
+      this.x += movement;
+      if (this.x >= this.height) {
+        this.x = 0;
+        this.y = Math.random() * this.width;
+      }
+    }
+
   public draw() {
     this.ctx.beginPath();
     this.ctx.fillStyle = this.mappedImage[1][this.position1][this.position2];
@@ -56,65 +72,4 @@ export class Particle {
   public getSpeed(): number {
     return this.speed;
   }
-}
-
-export class ParticleText {
-  protected x: number;
-  protected y: number;
-  protected size: number;
-  protected ctx: CanvasRenderingContext2D;
-  protected _2PI: number;
-  protected baseX: number;
-  protected baseY: number;
-  protected density: number;
-  protected mappedImage: any[][][];
-  
-  constructor(x: number, y: number, screenCanvas?: CanvasRenderingContext2D,
-    mapImg?: number[][][]) {
-    this.ctx = screenCanvas;
-    this.x = x;// + 200;
-    this.y = y;// - 100,
-    this.size = 2;
-    this.baseX = this.x;
-    this.baseY = this.y;
-    this.density = ((Math.random() * 30) + 1);
-    this._2PI = Math.PI * 2;
-    this.mappedImage = mapImg;
-  }
-
-  public update(mouse: any) {
-    let dx = mouse.x - this.x;
-    let dy = mouse.y - this.y;
-    let distance = Math.sqrt(dx*dx + dy*dy);
-    let forceDirectionX = dx / distance;
-    let forceDirectionY = dy / distance;
-    var maxDistance = mouse.radius;
-    var force = (maxDistance - distance) / maxDistance;
-
-    let directionX = (forceDirectionX * force * this.density);
-    let directionY = (forceDirectionY * force * this.density);
-    
-    if (distance < mouse.radius) {
-      this.x -= directionX ;
-      this.y -= directionY ;
-    }
-    else {
-      if (this.x !== this.baseX ) {
-          let dx = this.x - this.baseX;
-          this.x -= dx/5;
-      } if (this.y !== this.baseY) {
-          let dy = this.y - this.baseY;
-          this.y -= dy/5;
-      }
-    }
-  }
-
-  public draw() {
-    this.ctx.fillStyle = 'red';
-    this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, this.size, 0, this._2PI);
-    this.ctx.closePath();
-    this.ctx.fill();
-  }
-
 }
